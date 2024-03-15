13a67dc310f04fd23903b25415e40784d6f75dbc
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-03-15 19:46:19 -0400

    edited vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'PlayStation-Trivia-App'  // Set your desired production public path
+    : '/',
+  transpileDependencies: true,
 })
