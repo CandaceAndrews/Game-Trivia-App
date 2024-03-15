912138682cbbff9eb2a547185cfeaeeb63f164e2
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-03-14 21:27:08 -0400

    starting quiz question display

--- game-trivia-app/src/views/QuizPage.vue
+++ game-trivia-app/src/views/QuizPage.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="game-quiz-page">
         <h1 class="game-title">{{ game.title }}</h1>
+        <div v-if="currentQuestion">
+            <div class="question">
+                {{ currentQuestion.question }}
+            </div>
+            <div class="options">
+                <button v-for="(option, index) in currentQuestion.options" :key="index" @click="checkAnswer(option)">
+                {{ option }}
+                </button>
+            </div>
+        </div>
+        <div v-else>
+            <p>
+                All questions answered!
+            </p>
+        </div>
     </div>
 </template>
 
@@ -9,19 +24,51 @@ export default {
     props: {
         game: Object,
     },
-}
+    data() {
+        return {
+            currentQuestionIndex: 0,
+        };
+    },
+    computed: {
+        currentQuestion() {
+            if (this.game.questions && this.game.questionslenght > this.currentQuestionIndex) {
+                return this.game.questions[this.currentQuestionIndex];
+            }
+            return null;
+        },
+    },
+    methods: {
+        checkAnswer(option) {
+            const correctAnswer = this.currentQuestion.correctAnswer;
+            if (option === correctAnswer) {
+                alert('Correct');
+            } else {
+                alert('Incorrect!');
+            }
+            this.moveToNextQuestion();
+        },
+        moveToNextQuestion() {
+            this.currentQuestionIndex++;
+        },
+    },
+};
 </script>
 
 <style scoped>
-/* .home-page {
-    margin-top: 75px;
-    font-family: 'Press Start 2P', sans-serif;
-    font-size: 12px;
-    color: white;
-    background-color: black;
-} */
-
 .game-title {
     color: white;
+}
+
+.question {
+    margin-bottom: 20px;
+}
+
+.options {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.options button {
+    margin: 5px;
 }
 </style>
