9fe816533f51d875611e87a88724285bd73e893a
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-03-21 18:07:17 -0400

    working on storing correct answers

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,7 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-<<<<<<< HEAD
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'PlayStation-Trivia-App'  // Set your desired production public path
-    : '/',
-  transpileDependencies: true,
-})
-=======
   publicPath: './', // Use relative paths for assets
   transpileDependencies: true
 })
 
-
-// const { defineConfig } = require('@vue/cli-service')
-
-// module.exports = defineConfig({
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? 'PlayStation-Trivia-App'  // Set your desired production public path
-//     : '/',
-//   transpileDependencies: true,
-// })
->>>>>>> Github-Pages
